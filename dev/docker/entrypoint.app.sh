f29b65a6a7928b9cdc13713ed4e0b1701c61671c
ztf <ztfcharlie@163.com>
2025-08-13 23:10:11 +0800

    修改指引的数据库不对

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -8,7 +8,7 @@ if [[ -n "$1" ]]; then
     exec "$@"
 else
     composer install
-    wait-for-it db:3306 -t 45
+    wait-for-it burncloud-docs-db:3306 -t 45
     php artisan migrate --database=mysql --force
     chown -R www-data storage public/uploads bootstrap/cache
     exec apache2-foreground
