f9835170a023ed24ed15555c6270252b5ec9c0f5
ztf <ztfcharlie@163.com>
2025-08-13 23:57:13 +0800

    修改了，docker-compose文件的配置，由.env提供，以后该.env的配置就行

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Create required directories and set permissions
+mkdir -p /app/burncloud_docs_mysql_data
+mkdir -p /app/public/dist
+mkdir -p /app/node_modules
+chmod -R 777 /app/burncloud_docs_mysql_data
+chmod -R 777 /app/public/dist
+chmod -R 777 /app/node_modules
+chmod -R 777 /app/storage/logs
+
 env
 
 if [[ -n "$1" ]]; then
